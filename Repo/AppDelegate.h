9324888ff6f58240c008715be42b5884070091a9
demotesting92 <demotesting92@gmail.com>
2012-08-06 12:36:44 +0530

    Initial Commit

---
+++ Repo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Repo
+//
+//  Created by Vishal Shah on 06/08/12.
+//  Copyright (c) 2012 support@arihantsolutions.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
